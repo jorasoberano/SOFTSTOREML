e4dbdb4fc69f63b0e490236f1fea9dd05747fb7b
rulosob82082 <rulosob82082@gmail.com>
2023-03-11 04:10:30 -0600

    msg

--- mpago/revolutionsoft_transfspei2.php
+++ mpago/revolutionsoft_transfspei2.php
@@ -17,7 +17,7 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=utf-8\r\n"; 
 	
 if(mail($from, $asunto, $mensaje,$headers)){
-	header("Location: https://www.revolutionsoft.ml/mpago/checkout_v1_payment_redirect_0a5dbcbb-bec7-4d9e-a63d-269317184802_payment-option-form__source=Finalizá_tu_Pago_transfer_spei?item_name=$item_name&amount=$amount");
+	header("Location: https://www.revolutionsoft.ml/mpago/checkout_v1_payment_redirect_0a5dbcbb-bec7-4d9e-a63d-269317184802_payment-option-form__source=Finalizá_tu_Pago_transfer_spei?item_name=$item_name&amount=$amount&currency_code=$currency_code");
 exit();
 	}
 
